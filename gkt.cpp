3880833acc199f02a6451fdb3677981d7796cea0
Mohit Juneja <mohit0920@icloud.com>
2020-04-19 19:06:15 +0530

    google kickstart template for gkt.cpp

---
+++ gkt.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+
+// google kickstart template for faster code execution.
+
+using namespace std;
+int main() {
+//	printf("Template for competitive coding.\n");
+	int t,i;
+	int op[101];	// 100 is max testcases here
+	cin>>t;
+	
+	
+	for(i=0; i<t; i++){
+		// code for eveytest case.
+
+		
+	}	
+
+	// code to print output
+ 	for (i=0; i<t;i++){
+		cout<<"Case #"<<i+1<<": "<<op[i]<<"\n";
+	}
+	return 0;
+}
